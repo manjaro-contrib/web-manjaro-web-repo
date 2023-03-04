c7d96b76a7607812bf0a0dc649f431cc175ded8d
Philip Müller <philm@manjaro.org>
2023-03-04 11:27:04 +0700

    [mirrors] remove mirror.d-tl.com - see also: https://forum.manjaro.org/t/135488/4

--- src/conf.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-MIRRORS_URL = "https://repo.manjaro.org/mirrors.json"
-BRANCHES = ("stable", "testing", "unstable")
-ROOT_FOLDER = "/var/www/manjaro-web-repo/"
-OUTPUT_FOLDER = "docs/"
-LOGS_FOLDER = "logs/"
