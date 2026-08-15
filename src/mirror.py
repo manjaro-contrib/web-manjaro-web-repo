91553b8d1fdfbc8b8275466851bf4aa23e065a6f
Frede H <fh@manjaro.org>
2026-08-15 15:38:07 +0200

    Edit mirror.py
    The method logger.error will default to exiting script
    The third argument should be set to False to prevent the script
    from exiting prematurely

--- src/mirror.py
+++ src/mirror.py
@@ -37,12 +37,12 @@ class Mirror:
             try:
                 date = self.state_file.split("date=", 1)
                 if len(date) < 2:
-                    self.logger.error(f"{self.url}: state file is not valid", "date not found")
+                    self.logger.error(f"{self.url}: state file is not valid", "date not found", False)
                     self.last_sync = -1
                     self.branches = [-1, -1, -1]
                     return
             except:
-                self.logger.error(f"{self.url}: state file is not valid")
+                self.logger.error(f"{self.url}: state file is not valid", "", False)
                 self.last_sync = -1
                 self.branches = [-1, -1, -1]
                 return
