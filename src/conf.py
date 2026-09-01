2f816b90fc95eef773666e7c93adc48985488761
Philip Mueller <philm@manjaro.org>
2026-09-01 11:56:54 +0200

    [src/conf.py] fix root_folder

--- src/conf.py
+++ src/conf.py
@@ -2,7 +2,7 @@
 
 MIRRORS_URL = "https://repo.manjaro.org/mirrors.json"
 BRANCHES = ("stable", "testing", "unstable")
-ROOT_FOLDER = "/var/www/manjaro-web-repo"
+ROOT_FOLDER = "/var/www/manjaro-web-repo/"
 OUTPUT_FOLDER = "docs/"
 LOGS_FOLDER = "logs/"
 UA = "ManjaroMirrorBot/1.0 (+https://repo.manjaro.org)"
